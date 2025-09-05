a02822dc8d04be21b082cfcdfbd14e57805d8f30
Bruno <onetech@MacBook-Air-de-Bruno.local>
2025-09-05 19:37:51 -0300

    exerciciodb

---
+++ db.js
@@ -0,0 +1,13 @@
+import { createConnection } from "mysql2";
+
+const conecta = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "exerciciodb"
+});
+
+conecta.connect(function (err) {
+  if (err) throw err;
+  console.log("Conectado ao banco de dados!");
+});
